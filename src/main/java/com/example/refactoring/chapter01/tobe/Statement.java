1292bb1e9701066047c35504ea5f5dd4958e9b5e
jonghunbaek <qor6028@naver.com>
2025-01-17 08:50:36 +0900

    refactor : play 임시 변수 질의 함수로 변경

--- src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
+++ src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
@@ -6,21 +6,20 @@ public class Statement {
         int volumeCredit = 0;
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
         for (Performance performance : invoice.getPerformances()) {
-            Play play = plays.get(performance);
             int thisAmount = 0;
 
-            thisAmount = amountFor(performance, play);
+            thisAmount = amountFor(performance, plays);
 
             // 포인트를 적립한다.
             volumeCredit += Math.max(performance.getAudience() - 30, 0);
 
-            // 희극 관객 5명마다 추가 포인트를 제공핟나.
-            if (play.getType().equals("comedy")) {
+            // 희극 관객 5명마다 추가 포인트를 제공한다.
+            if (playFor(plays, performance).getType().equals("comedy")) {
                 volumeCredit += Math.floor(performance.getAudience() / 5);
             }
 
             // 청구 내역을 출력한다.
-            result.append(String.format("%s: $%d %d석\n",play.getName(), thisAmount / 100, performance.getAudience()));
+            result.append(String.format("%s: $%d %d석\n",playFor(plays, performance).getName(), thisAmount / 100, performance.getAudience()));
             totalAmount += thisAmount;
         }
 
@@ -29,9 +28,9 @@ public class Statement {
         return result.toString();
     }
 
-    private static int amountFor(Performance performance, Play play) {
+    private int amountFor(Performance performance, Plays plays) {
         int result;
-        switch (play.getType()) {
+        switch (playFor(plays, performance).getType()) {
             case "tragedy":
                 result = 40000;
                 if (performance.getAudience() > 30) {
@@ -49,6 +48,10 @@ public class Statement {
                 throw new RuntimeException("알 수 없는 장르");
         }
         return result;
+    }
+
+    private Play playFor(Plays plays, Performance performance) {
+        return plays.get(performance);
     }
 
     public String htmlStatement(Invoice invoice, Plays plays) {
