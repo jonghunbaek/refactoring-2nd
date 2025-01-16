12e9ff8dccce96d377a7a744ad8603c4c630879c
jonghunbaek <qor6028@naver.com>
2025-01-17 08:44:11 +0900

    refactor : switch문 함수로 추출

--- src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
+++ src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
@@ -9,23 +9,7 @@ public class Statement {
             Play play = plays.get(performance);
             int thisAmount = 0;
 
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (performance.getAudience() > 30) {
-                        thisAmount += 1000 * (performance.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (performance.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-                    }
-                    thisAmount += 300 * performance.getAudience();
-                    break;
-                default:
-                    throw new RuntimeException("알 수 없는 장르");
-            }
+            thisAmount = amountFor(performance, play);
 
             // 포인트를 적립한다.
             volumeCredit += Math.max(performance.getAudience() - 30, 0);
@@ -43,6 +27,28 @@ public class Statement {
         result.append(String.format("총액: $%d\n",totalAmount / 100));
         result.append(String.format("적립 포인트: %d점", volumeCredit));
         return result.toString();
+    }
+
+    private static int amountFor(Performance performance, Play play) {
+        int thisAmount;
+        switch (play.getType()) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (performance.getAudience() > 30) {
+                    thisAmount += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (performance.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                thisAmount += 300 * performance.getAudience();
+                break;
+            default:
+                throw new RuntimeException("알 수 없는 장르");
+        }
+        return thisAmount;
     }
 
     public String htmlStatement(Invoice invoice, Plays plays) {
