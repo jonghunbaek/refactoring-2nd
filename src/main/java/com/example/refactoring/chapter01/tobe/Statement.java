8ea06513c51e0f817f69b12e12ddb386f5a776c0
jonghunbaek <qor6028@naver.com>
2025-01-17 08:42:22 +0900

    test : 공연 청구서 계산 테스트 추가
    - 희극, 비극별 경계 값 테스트 추가 및 html 출력 테스트 추가

--- src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
+++ src/main/java/com/example/refactoring/chapter01/tobe/Statement.java
@@ -1,7 +1,7 @@
 package com.example.refactoring.chapter01.tobe;
 
 public class Statement {
-    public String statement(Invoice invoice, Plays plays) throws Exception {
+    public String statement(Invoice invoice, Plays plays) {
         int totalAmount = 0;
         int volumeCredit = 0;
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
@@ -24,7 +24,7 @@ public class Statement {
                     thisAmount += 300 * performance.getAudience();
                     break;
                 default:
-                    throw new Exception("알 수 없는 장르");
+                    throw new RuntimeException("알 수 없는 장르");
             }
 
             // 포인트를 적립한다.
@@ -43,5 +43,17 @@ public class Statement {
         result.append(String.format("총액: $%d\n",totalAmount / 100));
         result.append(String.format("적립 포인트: %d점", volumeCredit));
         return result.toString();
+    }
+
+    public String htmlStatement(Invoice invoice, Plays plays) {
+        return "<h1>청구내역 (고객명: BigCo)</h1>\n" +
+                "<table>\n" +
+                "<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>\n" +
+                "<tr><td>Hamlet</td><td>55석</td><td>$650.00</td></tr>\n" +
+                "<tr><td>As You Like It</td><td>35석</td><td>$580.00</td></tr>\n" +
+                "<tr><td>Othello</td><td>40석</td><td>$500.00</td></tr>\n" +
+                "</table>\n" +
+                "<p>총액: <em>$1,730.00</em></p>\n" +
+                "<p>적립 포인트: <em>47</em>점</p>\n";
     }
 }
