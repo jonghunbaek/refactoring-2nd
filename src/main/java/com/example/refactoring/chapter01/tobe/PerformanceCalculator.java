a2804a6c9dec5bcaa345edae138129c8677b9b8d
jonghunbaek <qor6028@naver.com>
2025-01-18 19:57:22 +0900

    refactor : 공연 요금 계산 메서드 부모 클래스에서 구현 제거

--- src/main/java/com/example/refactoring/chapter01/tobe/PerformanceCalculator.java
+++ src/main/java/com/example/refactoring/chapter01/tobe/PerformanceCalculator.java
@@ -11,27 +11,7 @@ public class PerformanceCalculator {
     }
 
     public int amountFor() {
-        int result;
-
-        switch (play.getType()) {
-            case "tragedy":
-                result = 40000;
-                if (performance.getAudience() > 30) {
-                    result += 1000 * (performance.getAudience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (performance.getAudience() > 20) {
-                    result += 10000 + 500 * (performance.getAudience() - 20);
-                }
-                result += 300 * performance.getAudience();
-                break;
-            default:
-                throw new RuntimeException("알 수 없는 장르");
-        }
-
-        return result / 100;
+        return 0;
     }
 
     public int volumeCreditFor() {
